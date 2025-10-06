45fc9af7b633e60c5c48fcea59faeda530e95586
Matthias Pfeifer <mpfeifer77@gmail.com>
2025-10-06 09:30:37 -0400

    Make script use sqlite3 instead of mysql. Redo php script to look nicer.

--- spiderman.php
+++ spiderman.php
@@ -1,54 +1,238 @@
-<!doctype html>
-<html lang="en">
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Spiderman discovered hosts</title>
-<meta name="description" content="List of hosts discovered by spiderman">
-<meta name="author" content="Matthias">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="spiderman.css?v=1.0">
-</head>
-
-<body>
-<h1>List of discovered hosts</h1>
-
-<div>
-This is the list of hosts yet discovered by spiderman web spider.
-</div>
 <?php
-$db_ini = parse_ini_file("conf/database.ini");
-$servername = $db_ini["servername"];
-$username = $db_ini["username"];
-$password = $db_ini["password"];
-$dbname = $db_ini["dbname"];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+session_start();
+
+$databaseFile = 'spiderman.db';
+
+try {
+    if (!isset($_SESSION['logbook'])) {
+        $_SESSION['logbook']=[];
+        $_SESSION['logbook'][] = [time(), "Session started"];
+    }
+    if (!isset($_SESSION['hosts'])) {
+        $_SESSION['logbook'][] = [time(), "Getting hosts from the database and storing hosts in session"];
+        $db = new SQLite3($databaseFile);
+        $results = $db->query('SELECT * FROM hosts');
+        $_SESSION['hosts'] = [];
+        while ($host = $results->fetchArray(SQLITE3_ASSOC)) {
+            $_SESSION['hosts'][] = $host;
+        }
+        $db->close();
+    }
+} catch (Exception $e) {
+    $_SESSION['logbook'][] = [time(), $e->getMessage()];
 }
 
-$sql = "SELECT * FROM hosts where active=1 ORDER BY timestamp DESC";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  echo "<table class='table'>";
-  echo "<tr><th>Timestamp</th><th>IPv4</th><th>Port</th><th>Hostname</th><tr>";
-  echo "<tbody>";
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["timestamp"] . "</td><td><a href=\"http://" . $row["ip"] . ":" . $row["port"] . "\">" . $row["ip"] . "</a></td><td> " . $row["port"] . "</td><td>" . $row["hostname"] . "</td></tr>";
-  }
-  echo "</tbody>";
-} else {
-  echo "<div>No hosts discovered</div>";
-}
-$conn->close();
 ?>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<script src="spiderman.js"></script>
-</body>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="index.ico">
+    <link rel="apple-touch-icon" href="index.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Matthias">
+    <meta name="description" content="Just trying to connect to port 80 on random osts via ipv4.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:description" content="Just trying to connect to port 80 on random osts via ipv4.">
+    <meta property="og:image" content="index.png">
+    <meta property="og:title" content="Search for random http server.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="add-url-here">
+    <title>Spiderman-UI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+      table {
+          table-layout: fixed;
+          width: 100%; /* Optional: set a width for the entire table */
+      }
+
+      @media only screen and (orientation:portrait) {
+          .hide-on-mobile {
+              display: none;
+          }
+      }
+
+      @media only screen and (orientation:landscape) {
+
+          th:nth-child(1), td:nth-child(1) {
+              width: 3em; /* Specific width for the first column */
+          }
+
+          th:nth-child(2), td:nth-child(2) {
+              width: 10em;
+          }
+
+          th:nth-child(4), td:nth-child(4) {
+              width: 10em;
+          }
+
+      }
+      
+      tr {
+          height: 2.5em;
+      }
+
+      td div {
+          height: 2.5em; /* Adjust to fit within the row height */
+          overflow: hidden; /* Hide overflowing content */
+          white-space: normal; /* Allow text to wrap */
+          text-overflow: ellipsis; /* Add ellipsis for hidden text */
+      }
+    </style>
+  </head>
+
+  <body>
+    <script>
+      var hosts = <?php echo json_encode($_SESSION['hosts'])?>;
+
+      let prevPage=1;
+      let nextPage=2;
+      let hostsPerPage = 5;
+      let currentPage=1;
+
+      function gotoPage(page) {
+          if (page != currentPage && ((page-1) * hostsPerPage) < hosts.length) {
+            fillTable(page);
+            currentPage = page;
+            nextPage = currentPage+1;
+            prevPage = Math.max(1, currentPage-1);
+            }
+            }
+
+            function fillTable(pageIndex) {
+            let from = (pageIndex-1) * hostsPerPage;
+            let to = pageIndex * hostsPerPage;
+            let data = hosts.slice(from, to);
+            var tableBody = document.getElementById("hosts-table");
+          tableBody.innerHTML = '';
+
+          data.forEach(rowData => {
+              var tr = document.createElement("tr");
+              var td = document.createElement("td");
+              var div = document.createElement("div");
+              div.textContent = rowData['id'];
+              td.appendChild(div);
+              tr.appendChild(td);
+              div = document.createElement("div");
+              td = document.createElement("td");
+              td.className='hide-on-mobile';
+              const dateObject = new Date(rowData['timestamp']*1000);
+              div.textContent = dateObject.toLocaleString();
+              td.appendChild(div);
+              tr.appendChild(td);
+              div = document.createElement("div");
+              td = document.createElement("td");
+              td.className='hide-on-mobile';
+              div.textContent = rowData['hostname'];
+              td.appendChild(div);
+              tr.appendChild(td);
+              div = document.createElement("div");
+              td = document.createElement("td");
+              const newLink = document.createElement('a');
+              newLink.href = `http://${rowData['ip']}`;
+              newLink.textContent = rowData['ip'];
+              newLink.target = '_blank'; // Optional: Opens the link in a new tab
+              newLink.className = 'btn btn-primary w-100'; // Optional: Add Bootstrap classes
+              div.appendChild(newLink);
+              td.appendChild(div);
+              tr.appendChild(td);
+              tableBody.appendChild(tr);
+          });
+
+          if (to > hosts.length) {
+              for (let i=hosts.length; i<to; i++) {
+                  var tr = document.createElement("tr");
+                  var td = document.createElement("td");
+                  var div = document.createElement("div");
+                  div.textContent = i;
+                  td.appendChild(div);
+                  tr.appendChild(td);
+                  div = document.createElement("div");
+                  td = document.createElement("td");
+                  td.className='hide-on-mobile';
+                  div.textContent = "";
+                  td.appendChild(div);
+                  tr.appendChild(td);
+                  div = document.createElement("div");
+                  td = document.createElement("td");
+                  td.className='hide-on-mobile';
+                  div.textContent = "";
+                  td.appendChild(div);
+                  tr.appendChild(td);
+                  div = document.createElement("div");
+                  td = document.createElement("td");
+                  div.textContent = "";
+                  td.appendChild(div);
+                  tr.appendChild(td);
+                  tableBody.appendChild(tr);
+              }
+          }
+
+      }
+</script>
+    <main>
+      <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+          <img width="64" height="64" src="cartoon-spider.png" />
+          <span class="fs-4">spiderman.py</span>
+        </header>
+
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5">
+            <div class="row">
+              <div class="col-md-8">
+                <p class="fs-4"><a href="https://github.com/mpfeifer/spiderman.py">Spiderman</a> is a <a href="https://www.python.org/">Pyhton</a> script that is connecting to port 80 on random hosts infinitly. If the connection attempt succeeds, it assumes that there is a webserver listening on that hosts port 80. Those hosts ip addresses are written down in an sqlite3 database file. This file's content is presented in this UI using the <a href="http://www.getbootstrap.com">Bootstrap 5.2 CSS-Framework</a> and <a href="http://www.php.net">PHP 8.4</a>.</p>
+              </div>
+              <div class="col-md-4 d-flex">
+                <img src="spiderman.png" class="img-fluid rounded-3" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row align-items-md-stretch">
+          <div class="col-md-12">
+            <div class="h-100 p-5 bg-light border rounded-3">
+              <h2>Recently discovered servers</h2>
+
+              <table class="table table-striped table-md">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col" class="hide-on-mobile">Timestamp</th>
+                    <th scope="col" class="hide-on-mobile">Hostname</th>
+                    <th scope="col">IPv4</th>
+                  </tr>
+                </thead>
+                <tbody id="hosts-table">
+                  <!-- filled by fillTable function -->
+                </tbody>
+              </table>
+
+              <button class="btn btn-outline-secondary" onclick="gotoPage(prevPage)" type="button">Prev Page</button>
+              <button class="btn btn-outline-secondary" onclick="gotoPage(nextPage)" type="button">Next Page</button>
+              
+            </div>
+          </div>
+
+          <footer class="pt-3 mt-4 text-muted border-top">
+            &copy; Matthias 2025
+          </footer>
+        </div>
+    </main>
+
+    <article>
+    </article>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script>
+      fillTable(currentPage);
+    </script>
+  </body>
+
+</html>
--- spiderman.py3
+++ spiderman.py3
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
-from MySQLdb import _mysql
 from time import time
 
-import configparser
 import ipaddress
+import logging
 import random
 import socket
 import sys
+import threading
+import os
+
+import sqlite3
+
+logging.basicConfig(format='%(asctime)s|%(levelname)s|%(message)s',
+                    filename='spiderman.log', level=logging.INFO)
+
+log = logging.getLogger(__name__)
+
+db_file = 'spiderman.db'
+
+if not os.path.isfile(db_file):
+    log.info(f"Initializing sqlite database in {db_file}")
+    db_conn = sqlite3.connect(db_file)
+    cursor = db_conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS `hosts` (
+        id INTEGER PRIMARY KEY,
+        ip TEXT NOT NULL,
+        timestamp INTEGER NOT NULL,
+        port INTEGER,
+        hostname TEXT
+    )
+    ''')
+    db_conn.close()
 
 class Tools:
-
     @staticmethod
     def friendly_hostname(ipv4):
         try: 
@@ -22,63 +46,52 @@ class Tools:
         return ""
 
 class Factory:
-
     @staticmethod
     def hostDescriptor(ipv4, port):
         return {
             'ipv4': ipv4,
             'timestamp': time(),
             'port': port,
-            'hostname': Tools.friendly_hostname(ipv4),
-            'active': False
+            'hostname': Tools.friendly_hostname(ipv4)
         }
-
     @staticmethod
     def randomIpv4():
         MAX_IPV4=ipaddress.IPv4Address._ALL_ONES
         while True:
             randomInt = random.randint(0, MAX_IPV4)
-            if ((randomInt >= 2886729728) && (randomInt <= 2887778303) or
-                (randomInt >= 3232235520) && (randomInt <= 3232301055) or
-                (randomInt >=  167772160) && (randomInt <=  184549375)):
+            if ((randomInt >= 2886729728) and (randomInt <= 2887778303) or
+                (randomInt >= 3232235520) and (randomInt <= 3232301055) or
+                (randomInt >=  167772160) and (randomInt <=  184549375)):
                 continue
             else:
                 break
         return ipaddress.IPv4Address._string_from_ip_int( randomInt )
-
     @staticmethod
     def ipv4Pool():
         while True:
             yield Factory.randomIpv4();
 
-foundServers = []
-noResponseHosts = []
-sockets = []
-dbhost, dgbuser, dbpasswd, dbname = None, None, None, None
-
-try:
-    parser=configparser.ConfigParser()
-    parser.read("conf/spiderman.conf")
-    dbhost=parser.get("spiderman", "dbhost")
-    dbuser=parser.get("spiderman", "dbuser")
-    dbpasswd=parser.get("spiderman", "dbpasswd")
-    dbname=parser.get("spiderman", "dbname")
-except KeyError as e:
-    print("Configuration file is missing values. Please configure dbhost, dbuser, dbpasswd and dbname")
-    sys.exit(1)
-
 for target in Factory.ipv4Pool():
-
+    hostDescriptor = None
+    hostDescriptor = Factory.hostDescriptor(target, 80)
+    ip=hostDescriptor['ipv4']
+    log.info(f"Trying connection to host {ip}")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        hostDescriptor = None
-        sockets.append(s)
-        s.settimeout(15.0)
-        hostDescriptor = Factory.hostDescriptor(target, 80)
         try:
+            s.settimeout(5.0)
             s.connect((target, 80))
             s.shutdown(socket.SHUT_RDWR)
-            hostDescriptor['active']=True
+            log.info(f"Server found with ip {ip}")
+            db_conn = sqlite3.connect(db_file)
+            cursor = db_conn.cursor()
+            data_tuple = (
+                hostDescriptor['ipv4'],
+                hostDescriptor['timestamp'],
+                hostDescriptor['port'],
+                hostDescriptor['hostname']
+            )
+            cursor.execute(sql, data_tuple)
+            db_conn.commit()
+            db_conn.close()
         except OSError:
-            pass
-        db=_mysql.connect(host=dbhost, user=dbuser, passwd=dbpasswd, db=dbname)
-        db.query(f"INSERT INTO hosts VALUES('{hostDescriptor['ipv4']}', FROM_UNIXTIME({hostDescriptor['timestamp']}), '{hostDescriptor['port']}', '{hostDescriptor['hostname']}', '{int(hostDescriptor['active'])}')")
+            pass # ran into timeout
