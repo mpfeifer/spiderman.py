bfc4b672bee325e3f2d1914cb32abc1892b62cec
Matthias Pfeifer <mpfeifer77@gmail.com>
2025-10-09 13:59:28 -0400

    Added chart.js

--- spiderman.php
+++ spiderman.php
@@ -45,9 +45,14 @@ try {
     <title>Spiderman-UI</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
+
       table {
           table-layout: fixed;
           width: 100%; /* Optional: set a width for the entire table */
+      }
+
+      table * {
+          font-size: 10pt;
       }
 
       @media only screen and (orientation:portrait) {
@@ -63,7 +68,7 @@ try {
           }
 
           th:nth-child(2), td:nth-child(2) {
-              width: 10em;
+              width: 13em;
           }
 
           th:nth-child(4), td:nth-child(4) {
@@ -73,15 +78,18 @@ try {
       }
       
       tr {
-          height: 2.5em;
+          height: 3.0em;
       }
 
       td div {
-          height: 2.5em; /* Adjust to fit within the row height */
+          height: 3.0em;  Adjust to fit within the row height */
           overflow: hidden; /* Hide overflowing content */
           white-space: normal; /* Allow text to wrap */
           text-overflow: ellipsis; /* Add ellipsis for hidden text */
+          padding: 0;
+          margin: 0;
       }
+
     </style>
   </head>
 
@@ -91,23 +99,35 @@ try {
 
       let prevPage=1;
       let nextPage=2;
-      let hostsPerPage = 5;
+      let hostsPerPage = 15;
       let currentPage=1;
 
+      function ipv4octets(ipv4) {
+          let result = [0, 0, 0, 0];
+          let p1 = ipv4.indexOf('.');
+          let p2 = ipv4.indexOf('.', p1+1);
+          let p3 = ipv4.indexOf('.', p2+1);
+          result[0] = ipv4.substring(0, p1);
+          result[1] = ipv4.substring(p1+1, p2);
+          result[2] = ipv4.substring(p2+1, p3);
+          result[3] = ipv4.substring(p3+1);
+          return result;
+      }
+      
       function gotoPage(page) {
           if (page != currentPage && ((page-1) * hostsPerPage) < hosts.length) {
-            fillTable(page);
-            currentPage = page;
-            nextPage = currentPage+1;
-            prevPage = Math.max(1, currentPage-1);
-            }
-            }
+              fillTable(page);
+              currentPage = page;
+              nextPage = currentPage+1;
+              prevPage = Math.max(1, currentPage-1);
+          }
+      }
 
-            function fillTable(pageIndex) {
-            let from = (pageIndex-1) * hostsPerPage;
-            let to = pageIndex * hostsPerPage;
-            let data = hosts.slice(from, to);
-            var tableBody = document.getElementById("hosts-table");
+      function fillTable(pageIndex) {
+          let from = (pageIndex-1) * hostsPerPage;
+          let to = pageIndex * hostsPerPage;
+          let data = hosts.slice(from, to);
+          var tableBody = document.getElementById("hosts-table");
           tableBody.innerHTML = '';
 
           data.forEach(rowData => {
@@ -136,7 +156,7 @@ try {
               newLink.href = `http://${rowData['ip']}`;
               newLink.textContent = rowData['ip'];
               newLink.target = '_blank'; // Optional: Opens the link in a new tab
-              newLink.className = 'btn btn-primary w-100'; // Optional: Add Bootstrap classes
+              newLink.className = 'btn btn-sm btn-primary w-100'; // Optional: Add Bootstrap classes
               div.appendChild(newLink);
               td.appendChild(div);
               tr.appendChild(td);
@@ -171,9 +191,35 @@ try {
                   tableBody.appendChild(tr);
               }
           }
-
       }
-</script>
+
+      const countsByOctet = [[], [], [], []];
+      const propabilitiesByOctet = [[], [], [], []];
+      const totalCount = hosts.length;
+
+      hosts.forEach( (host) => {
+          let ip = host['ip'];
+          let octets = ipv4octets(ip);
+          for (oi in [0, 1, 2, 3]) { // oi = octetIndex
+              if (typeof countsByOctet[oi][octets[oi]] !== "undefined") {
+                  countsByOctet[oi][octets[oi]] = countsByOctet[oi][octets[oi]] + 1;
+              } else {
+                  countsByOctet[oi][octets[oi]] = 1;
+              }
+          }
+      });
+
+      const dataForChartjs = [[], [], [], []];
+      for (oi in [0, 1, 2, 3]) { // oi = octetIndex
+          for ( i in countsByOctet[Number(oi)]) {
+              dataForChartjs[oi].push( {x: i, y:countsByOctet[oi][i]} );
+          }
+      }
+     
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <main>
       <div class="container py-4">
         <header class="pb-3 mb-4 border-bottom">
@@ -184,8 +230,9 @@ try {
         <div class="p-5 mb-4 bg-light rounded-3">
           <div class="container-fluid py-5">
             <div class="row">
+              <h1> About Spiderman</h1>
               <div class="col-md-8">
-                <p class="fs-4"><a href="https://github.com/mpfeifer/spiderman.py">Spiderman</a> is a <a href="https://www.python.org/">Pyhton</a> script that is connecting to port 80 on random hosts infinitly. If the connection attempt succeeds, it assumes that there is a webserver listening on that hosts port 80. Those hosts ip addresses are written down in an sqlite3 database file. This file's content is presented in this UI using the <a href="http://www.getbootstrap.com">Bootstrap 5.2 CSS-Framework</a> and <a href="http://www.php.net">PHP 8.4</a>.</p>
+                <p class="fs-4"><a href="https://github.com/mpfeifer/spiderman.py">Spiderman</a> is a <a href="https://www.python.org/">Pyhton</a> script that is infinitly connecting to random hosts on port 80. If an connection attempt succeeds, it assumes that there is a webserver listening on that host. Those host ip addresses are written down in an sqlite3 database file. This file's content is presented in this UI using the <a href="http://www.getbootstrap.com">Bootstrap 5.2 CSS-Framework</a> and <a href="http://www.php.net">PHP 8.4</a>.</p>
               </div>
               <div class="col-md-4 d-flex">
                 <img src="spiderman.png" class="img-fluid rounded-3" />
@@ -194,10 +241,12 @@ try {
           </div>
         </div>
 
+
         <div class="row align-items-md-stretch">
           <div class="col-md-12">
             <div class="h-100 p-5 bg-light border rounded-3">
-              <h2>Recently discovered servers</h2>
+
+              <h1>Recently discovered servers</h1>
 
               <table class="table table-striped table-md">
                 <thead>
@@ -218,11 +267,54 @@ try {
               
             </div>
           </div>
-
-          <footer class="pt-3 mt-4 text-muted border-top">
-            &copy; Matthias 2025
-          </footer>
         </div>
+
+        <div class="row align-items-md-stretch mt-4">
+
+          <div class="col-md-12">
+            <div class="h-100 p-5 bg-light border rounded-3">
+              <h1>Some Statistics</h1>
+              The numbers generated by the python script open doors to some stastic methods, whose results can then be visualized using <a href="http://chartjs.org/">chart.js</a>.
+            </div>
+          </div>
+        </div>
+        
+        <div class="row align-items-md-stretch mt-4 gy-4">
+
+          <div class="col-md-6">
+            <div class="h-100 p-2 bg-light border rounded-3">
+              <canvas id="chart_1"></canvas>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="h-100 p-2 bg-light border rounded-3">
+              <canvas id="chart_2"></canvas>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row align-items-md-stretch mt-4 gy-4">
+
+          <div class="col-md-6">
+            <div class="h-100 p-2 bg-light border rounded-3">
+              <canvas id="chart_3"></canvas>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="h-100 p-2 bg-light border rounded-3">
+              <canvas id="chart_4"></canvas>
+            </div>
+          </div>
+          
+        </div>
+
+        <footer class="pt-3 mt-4 text-muted border-top">
+          &copy; Matthias 2025
+        </footer>
+      </div>
     </main>
 
     <article>
@@ -231,8 +323,51 @@ try {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <script>
+      // Setup table with hosts information
       fillTable(currentPage);
+
+      // Setup charts
+
+      let ordinal = ["0th", "1st", "2nd", "3rd", "4th"];
+
+      for (let i=1; i<=4; i++) {
+          const ctx = document.getElementById(`chart_${i}`);
+          const text = `Propabilites by ${ordinal[i]} Octet of Ipv4 Address`;
+          new Chart(ctx, {
+              type: 'line',
+              data: {
+                  datasets: [{
+                      data: dataForChartjs[i-1],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  parsing: {
+                      xAxisKey: 'x',
+                      yAxisKey: 'y'
+                  },
+                  responsive: true,
+                  scales: {
+                      y: {
+                          beginAtZero: true
+                      }
+                  },
+                  plugins: {
+                      title: {
+                          display: true,
+                          text: text
+                      },
+                      legend: {
+                          display: false
+                      }
+                  }
+              }
+          });
+      }
     </script>
+
+
+
   </body>
 
 </html>
